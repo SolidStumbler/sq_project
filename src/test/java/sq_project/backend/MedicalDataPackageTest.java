a51c9ddc56f7776fe4198d56d369bb528f432a08
SolidStumbler <matalbertsen@gmail.com>
2019-08-22 17:51:54 +0200

    Create MedicalDataPackageTest.java

---
+++ src/test/java/sq_project/backend/MedicalDataPackageTest.java
@@ -0,0 +1,16 @@
+package sq_project.backend;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.*;
+
+public class MedicalDataPackageTest {
+    @Test
+    public void CreateSuccessfullyTest() throws FileNotFoundException {
+        CSVPackage val = CSVUtils.parseFile(System.getProperty("user.dir") + "/src/test/test_data/valid_csv.csv");
+        MedicalDataPackage data = MedicalDataPackage.createFromCSVPackage(val);
+        assert(data.size() == 62);
+    }
+}
