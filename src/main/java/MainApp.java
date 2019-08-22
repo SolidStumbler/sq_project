107ceecd268afede4da34dd5bcd528dab90ad1eb
roko1992 <robert.konietzke@icloud.com>
2019-08-22 13:58:52 +0200

    Update title of stage

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -16,6 +16,7 @@ public class MainApp extends Application {
 
         Scene scene = new Scene(root);
 
+        stage.setTitle("Verwaltungsbericht PMS");
         stage.setScene(scene);
         stage.show();
     }
