0bd2bd762b5d905d2a71b6983acf379350d99a88
SolidStumbler <matalbertsen@gmail.com>
2019-08-23 11:37:39 +0200

    Update CollectedExceptionsException.java

--- src/main/java/sq_project/backend/CollectedExceptionsException.java
+++ src/main/java/sq_project/backend/CollectedExceptionsException.java
@@ -1,15 +1,26 @@
 package sq_project.backend;
 
-import javax.xml.crypto.dsig.keyinfo.KeyValue;
 import java.util.*;
 
+/**
+ * An Exception containing a collection of other exceptions occuring during the parsing of a csv-file
+ */
 public class CollectedExceptionsException extends Exception {
     private Map<Integer, Exception> innerExceptions = new HashMap<Integer, Exception>();
 
+    /**
+     * Add new exception to the internal collection
+     * @param e new exception
+     * @param lineNumber line in which this exception occured
+     */
     public void add(Exception e, int lineNumber){
         innerExceptions.put(lineNumber, e);
     }
 
+    /**
+     * The Messages of the internal exceptions
+     * @return a string containing the messages of the internal exceptions
+     */
     @Override
     public String getMessage() {
         String retVal = "Collected Line Errors: ";
@@ -19,6 +30,10 @@ public class CollectedExceptionsException extends Exception {
         return retVal;
     }
 
+    /**
+     * Are there any internal Exceptions?
+     * @return true if there are any internal exceptions
+     */
     public boolean isEmpty(){
         return innerExceptions.isEmpty();
     }
